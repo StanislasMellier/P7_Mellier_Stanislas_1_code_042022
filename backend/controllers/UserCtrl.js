7acfce3e7e35ae1c56d47d113560ef942164c6e8
StanislasMellier <95698594+StanislasMellier@users.noreply.github.com>
2022-04-27 15:00:56 +0200

    Ajout Routes Get et check User
    Ajout des routes pour récupérer les données d'un user et vérifier sa validité

--- backend/controllers/UserCtrl.js
+++ backend/controllers/UserCtrl.js
@@ -1,13 +1,13 @@
 const Mysql = require('../config/Mysql');
 const bcrypt = require('bcrypt');
+const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const { v4: uuidv4 } = require('uuid');
 exports.register = (req, res, next) => {
-	// const email = req.body.email;
-	// const password = req.body.password;
-	// const name = req.body.name;
 	const { email, password, name } = req.body;
+	const userId = uuidv4();
 	if (!email || !password || !name) {
+		console.log(req.body);
 		return res
 			.status(400)
 			.json({ message: 'Paramètres email et/ou password manquant.' });
@@ -17,12 +17,18 @@ exports.register = (req, res, next) => {
 		.then((hash) => {
 			Mysql.query(
 				'INSERT INTO users (id,email,password,name) VALUES (?,?,?,?)',
-				[uuidv4(), email, hash, name],
+				[userId, email, hash, name],
 				(err, results) => {
 					if (err) {
-						return res.status(500).json({ err });
+						return res.status(500).json({
+							error: new Error('Internal Server Error'),
+						});
 					}
-					res.status(201).json({ message: 'Utilisateur enregistré' });
+					res.status(201).json({
+						message: 'Utilisateur enregistré',
+						userId: userId,
+						token: generateJWT(userId),
+					});
 				}
 			);
 		})
@@ -40,10 +46,9 @@ exports.login = (req, res, next) => {
 			}
 			if (!results[0]) {
 				return res
-					.status(401)
+					.status(400)
 					.json({ message: 'Utilisateur inexistant' });
 			}
-			console.log(results);
 			bcrypt
 				.compare(password, results[0].password)
 				.then((valid) => {
@@ -54,14 +59,100 @@ exports.login = (req, res, next) => {
 					}
 					res.status(200).json({
 						userId: results[0].id,
-						token: jwt.sign(
-							{ userId: results[0].id },
-							process.env.JWT_SECRET,
-							{ expiresIn: '24h' }
-						),
+						token: generateJWT(results[0].id),
 					});
 				})
 				.catch((err) => res.status(500).json({ err }));
 		}
 	);
 };
+
+exports.ChangeProfilePic = (req, res, next) => {
+	if (!req.file) {
+		return res
+			.status(400)
+			.json({ message: 'Veuillez joindre un fichier.' });
+	}
+	if (req.params.id !== req.auth.userId) {
+		return res
+			.status(403)
+			.json({ error: new Error('Requête non authoriser.') });
+	}
+	const userId = req.params.id;
+	Mysql.query(
+		'SELECT id,ProfilPicUrl FROM users WHERE id=?',
+		[userId],
+		(err, userData) => {
+			if (err) {
+				return res.status(500).json({ err });
+			}
+			if (!userData[0]) {
+				return res
+					.status(400)
+					.json({ message: 'Utilisateur inexistant' });
+			}
+
+			const imageUrl = req.file.filename;
+
+			Mysql.query(
+				'UPDATE users SET profilePicUrl=? WHERE id=?',
+				[imageUrl, userId],
+				(err, results) => {
+					if (err) {
+						return res.status(500).json({ err });
+					}
+					if (userData[0].profilePicUrl !== 'default-profile.jpg') {
+						console.log('not default');
+					}
+					res.status(200).json({
+						message: 'Photo de profil mis à jour',
+					});
+				}
+			);
+		}
+	);
+};
+
+exports.CheckUser = (req, res, next) => {
+	const { userId, token } = req.body;
+	// Vérifie si le userId est rattacher a un utilisateur existant
+	Mysql.query('SELECT 1 from users WHERE id = ?', [userId], (err, result) => {
+		if (err) {
+			return res
+				.status(400)
+				.json({ isValid: false, message: 'Utilisateur inexistant' });
+		}
+	});
+
+	const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+	if (userId !== decodedToken.userId) {
+		return res
+			.status(401)
+			.json({ isValid: false, message: 'Token invalide' });
+	} else {
+		return res.status(200).json({ isValid: true, message: 'Token valide' });
+	}
+};
+
+exports.GetUser = (req, res, next) => {
+	const userId = req.params.id;
+	Mysql.query(
+		'SELECT isAdmin,name,profilePicUrl from users WHERE id=?',
+		[userId],
+		(err, results) => {
+			if (err) {
+				return res.status(500).json({ error: err });
+			}
+			if (results) {
+				const user = results[0];
+				return res.status(200).json({ user });
+			}
+		}
+	);
+};
+
+function generateJWT(id) {
+	return jwt.sign({ userId: id }, process.env.JWT_SECRET, {
+		expiresIn: '14d',
+	});
+}
--- backend/routes/UsersRoute.js
+++ backend/routes/UsersRoute.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const UserCtrl = require('../controllers/UserCtrl');
 const multer = require('../middleware/multer');
+const auth = require('../middleware/auth');
 
 router.post('/register', UserCtrl.register);
 router.post('/login', UserCtrl.login);
 
-router.put('/profilpic', multer, UserCtrl.ChangeProfilePic);
+router.post('/check', UserCtrl.CheckUser);
+router.get('/:id', auth, UserCtrl.GetUser);
+router.put('/profilpic/:id', auth, multer, UserCtrl.ChangeProfilePic);
 module.exports = router;
